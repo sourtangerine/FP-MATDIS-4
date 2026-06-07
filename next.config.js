60f36a5858d9f238b28162dbe95618e85157ab96
sourtangerine <bimoktsr229@gmail.com>
2026-06-07 16:11:05 +0700

    fix: conditional standalone output for Docker only

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: process.env.DOCKER_BUILD === '1' ? 'standalone' : undefined,
 };
 
 module.exports = nextConfig;
